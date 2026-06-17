29d20b4953afbd6448290905315796ee57d68f25
abdullah-rajpoot01 <al.burhan.abn@gmail.com>
2026-06-17 11:55:47 +0500

    bg image addt

--- components/footer-1.tsx
+++ components/footer-1.tsx
@@ -12,7 +12,7 @@ interface FooterLinksSection {
   title: string
   links: FooterLink[]
 }
- 
+
 const Footer1 = () => {
   const searchParams = useSearchParams();
 
@@ -41,21 +41,23 @@ const Footer1 = () => {
   ];
 
   return (
-    <div className="px-6 w-full relative text-foreground">
-      <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-primary-foreground relative! overflow-hidden">
+    <div className="px-6 w-full relative text-white bg-black/90 bg-[url('/gym/pexels-binyaminmellish-17840.jpg')] bg-cover bg-top bg-fixed overflow-hidden min-h-[70vh] ">
+      <div className="absolute inset-0 bg-black/60 z-0" />
+
+      <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-primary-white relative! overflow-hidden z-10">
         <div className="">
           <a href={`/${createQuery({ gymName, city, phone })}`} className="inline-flex items-center justify-center gap-2  rounded-full ">
             <div className="flex items-center gap-3">
               <Avatar className="size-9">
                 {/* <AvatarImage alt="@shadcn" src="https://github.com/shadcn.png" /> */}
-                <AvatarFallback className="bg-black text-white uppercase">{gymName.charAt(0)}</AvatarFallback>
+                <AvatarFallback className="bg-white text-black uppercase">{gymName.charAt(0)}</AvatarFallback>
               </Avatar>
-              <div className="flex items-center gap-1 font-bold leading-none tracking-wider text-xl text-foreground">
+              <div className="flex items-center gap-1 font-bold leading-none tracking-wider text-xl text-white">
                 {gymName}
               </div>
             </div>
           </a>
-          <p className="max-w-102 mt-6 text-foreground/80">
+          <p className="max-w-102 mt-6 text-white/80">
             {gymName} Gym in {city} offers a safe and modern fitness space for both men and women. With separate areas, expert trainers, and affordable plans, it's the perfect place for cardio, strength training, and weight loss. Join now to start your fitness journey!
           </p>
         </div>
@@ -63,13 +65,13 @@ const Footer1 = () => {
         <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
           {linkSections.map((section, index) => (
             <div key={index}>
-              <h3 className="font-semibold text-base text-foreground md:mb-5 mb-2">
+              <h3 className="font-semibold text-base text-white md:mb-5 mb-2">
                 {section.title}
               </h3>
-              <ul className="text-sm space-y-1 text-foreground">
+              <ul className="text-sm space-y-1 text-white">
                 {section.links.map((link, i) => (
                   <li key={i}>
-                    <Link href={link.url} className="hover:underline transition text-foreground/80">
+                    <Link href={link.url} className="hover:underline transition text-white/80">
                       {link.label}
                     </Link>
                   </li>
@@ -79,7 +81,7 @@ const Footer1 = () => {
           ))}
         </div>
       </div>
-      <p className="py-4 text-center text-sm md:text-base text-foreground/80 border-t border-t-primary-foreground/50">
+      <p className="py-4 text-center text-sm md:text-base text-white/80 border-t border-t-primary-white/50 z-10 relative">
         Copyright 2026 © <Link href={`/${createQuery({ gymName, city, phone })}`}>{gymName} Gym .</Link> All
         Right Reserved.
       </p>
